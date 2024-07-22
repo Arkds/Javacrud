1a7fe46ae106e9c4dc9cd4397e62eba371094ddb
Arkds <arnolflo23@gmail.com>
2024-07-22 00:04:15 -0500

    subida

---
+++ src/historiaclinica/Main.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.Main to edit this template
+ */
+package historiaclinica;
+
+/**
+ *
+ * @author arnol
+ */
+public class Main {
+    public static void main(String[] args) {
+
+    /*frmMDI loginWin = new frmMDI();
+    loginWin.setVisible(true);*/
+    frmPresenta loginWin = new frmPresenta();
+    loginWin.setVisible(true);
+    }
+}
